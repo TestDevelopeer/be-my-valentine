f7f9ff9bea8b0de2a3c6253a85a89d332dbe1a5f
TestDevelopeer <testdeveloper7967@gmail.com>
2025-02-03 02:15:13 +0300

    change img path, remove scripts

--- main.js
+++ main.js
@@ -21,14 +21,14 @@ $(document).ready(function () {
 	}
 
 	$(document).on('click', '.no', function () {
-		$('.end-img').attr('src', '/img/sob_sticker.gif');
+		$('.end-img').attr('src', './img/sob_sticker.gif');
 		$('.foryou').html('Подумай еще раз')
 		intervalId = setInterval(showYes, 500);
 		$(this).addClass('op-0')
 	});
 
 	$(document).on('click', '.yes, .yyes', function () {
-		$('.end-img').attr('src', '/img/kissing_heart_sticker.gif');
+		$('.end-img').attr('src', './img/kissing_heart_sticker.gif');
 		$('.endtext-container').remove();
 		$('.yestext').removeClass('op-0');
 	})
