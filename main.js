d248c898f9e2e708b4ebd45d8da33810ae36ee22
johndoe <johndoe@example.com>
2025-02-04 15:46:36 +0300

    add background hearts

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ $(document).ready(function () {
 		if (index > 15) {
 			clearInterval(intervalId);
 		}
-		console.log('test');
 	}
 
 	$(document).on('click', '.no', function () {
@@ -31,5 +30,25 @@ $(document).ready(function () {
 		$('.end-img').attr('src', './img/kissing_heart_sticker.gif');
 		$('.endtext-container').remove();
 		$('.yestext').removeClass('op-0');
-	})
+	});
+
+	var love = setInterval(function () {
+		var r_num = Math.floor(Math.random() * 40) + 1;
+		var r_size = Math.floor(Math.random() * 65) + 10;
+		var r_left = Math.floor(Math.random() * 100) + 1;
+		var r_bg = Math.floor(Math.random() * 25) + 100;
+		var r_time = Math.floor(Math.random() * 5) + 5;
+
+		$('.bg_heart').append("<div class='heart' style='width:" + r_size + "px;height:" + r_size + "px;left:" + r_left + "%;background:rgba(255," + (r_bg - 25) + "," + r_bg + ",1);-webkit-animation:love " + r_time + "s ease;-moz-animation:love " + r_time + "s ease;-ms-animation:love " + r_time + "s ease;animation:love " + r_time + "s ease'></div>");
+		$('.bg_heart').append("<div class='heart' style='width:" + (r_size - 10) + "px;height:" + (r_size - 10) + "px;left:" + (r_left + r_num) + "%;background:rgba(255," + (r_bg - 25) + "," + (r_bg + 25) + ",1);-webkit-animation:love " + (r_time + 5) + "s ease;-moz-animation:love " + (r_time + 5) + "s ease;-ms-animation:love " + (r_time + 5) + "s ease;animation:love " + (r_time + 5) + "s ease'></div>");
+
+		$('.heart').each(function () {
+			var top = $(this).css("top").replace(/[^-\d\.]/g, '');
+			var width = $(this).css("width").replace(/[^-\d\.]/g, '');
+			console.log(width);
+			if (top <= -100 || width >= 150) {
+				$(this).detach();
+			}
+		});
+	}, 500);
 });
